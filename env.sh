677fe8fdbe843af9096e8fdc4ed631883a26484e
Admir Trakic <xomodo@gmail.com>
2017-10-18 21:09:19 +0200

    registry pass

--- env.sh
+++ env.sh
@@ -1,2 +1,5 @@
 DOCKER=$(basename "$PWD")
 TAG=$(git rev-parse --abbrev-ref HEAD) 
+
+REGISTRY_USER=$TAG
+REGISTRY_PASS=$TAG-12345
--- push.env.sh
+++ push.env.sh
@@ -1,11 +1,20 @@
 
-IMAGE_ID=$1
-VERSION=$2
-
-DOCKER=$(basename "$PWD")
 R_HOST=localhost
 R_PORT=6000
 
-# docker login $R_HOST:$R_PORT 
-docker tag $IMAGE_ID $R_HOST:$R_PORT/$DOCKER:$VERSION
+IMAGE_ID=$1
+VERSION=$2
+
+#VERSION=$(git rev-parse --short HEAD)
+#DOCKER=$(basename "$PWD")
+
+# echo "login" && docker login $R_HOST:$R_PORT 
+
+echo "tagging"  
+#docker tag $IMAGE_ID $R_HOST:$R_PORT/$DOCKER:$VERSION
+#docker tag  $DOCKER:latest $R_HOST:$R_PORT/$DOCKER:$VERSION
+docker tag  $DOCKER:$VERSION $R_HOST:$R_PORT/$DOCKER:$VERSION
+
+echo "pushing" 
 docker push $R_HOST:$R_PORT/$DOCKER:$VERSION 
+#docker push $R_HOST:$R_PORT/$DOCKER:newest 
