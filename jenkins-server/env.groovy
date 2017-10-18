062908f3863cd57a5173d28199cb6ba6075fe2c1
Admir Trakic <xomodo@gmail.com>
2017-10-18 22:45:08 +0200

    add groovy

---
+++ jenkins-server/env.groovy
@@ -0,0 +1,24 @@
+import hudson.slaves.EnvironmentVariablesNodeProperty
+import jenkins.model.Jenkins
+
+instance = Jenkins.getInstance()
+globalNodeProperties = instance.getGlobalNodeProperties()
+envVarsNodePropertyList = globalNodeProperties.getAll(EnvironmentVariablesNodeProperty.class)
+
+newEnvVarsNodeProperty = null
+envVars = null
+
+if ( envVarsNodePropertyList == null || envVarsNodePropertyList.size() == 0 ) {
+    newEnvVarsNodeProperty = new EnvironmentVariablesNodeProperty();
+      globalNodeProperties.add(newEnvVarsNodeProperty)
+        envVars = newEnvVarsNodeProperty.getEnvVars()
+} else {
+    envVars = envVarsNodePropertyList.get(0).getEnvVars()
+}
+
+envVars.put("MY_REGISTRY_HOST", "172.17.0.1")
+envVars.put("MY_REGISTRY_PORT", "5000")
+envVars.put("MY_REGISTRY_USER", "develop")
+envVars.put("MY_REGISTRY_PASS", "develop-12345")
+instance.save()
+
