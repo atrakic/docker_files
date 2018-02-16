ff9ca51e94908d806ad14f56b42e96c916d1e2d1
Admir Trakic <atr@skm.dk>
2018-02-16 15:59:12 +0100

    cadvisor Dockerfile update

--- cadvisor/run.env.sh
+++ cadvisor/run.env.sh
@@ -1,2 +1,12 @@
+#!/usr/bin/env bash
+
 . ../env.sh
-docker run --name $DOCKER -p 8090:8080 -v /var/run/docker.sock:/var/run/docker.sock:ro $DOCKER:$TAG
+
+docker run --name "$DOCKER" -p 8090:8080 \
+       -v /:/rootfs:ro \
+       -v /var/run:/var/run:rw \
+       -v /sys:/sys:ro  \
+       -v /var/run/docker.sock:/var/run/docker.sock:ro \
+       -d \
+       --restart always \
+	"$DOCKER:$TAG"
