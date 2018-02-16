8d0f267a4c152015f791517348133d4718498473
Admir Trakic <atr@skm.dk>
2018-02-16 16:08:59 +0100

    update

--- push.env.sh
+++ push.env.sh
@@ -1,20 +1,8 @@
 
-R_HOST=localhost
-R_PORT=6000
+. ../env.sh
 
-IMAGE_ID=$1
-VERSION=$2
+IMAGE_ID="$1"
+VERSION="$2"
 
-#VERSION=$(git rev-parse --short HEAD)
-#DOCKER=$(basename "$PWD")
-
-# echo "login" && docker login $R_HOST:$R_PORT 
-
-echo "tagging"  
-#docker tag $IMAGE_ID $R_HOST:$R_PORT/$DOCKER:$VERSION
-#docker tag  $DOCKER:latest $R_HOST:$R_PORT/$DOCKER:$VERSION
-docker tag  $DOCKER:$VERSION $R_HOST:$R_PORT/$DOCKER:$VERSION
-
-echo "pushing" 
-docker push $R_HOST:$R_PORT/$DOCKER:$VERSION 
-#docker push $R_HOST:$R_PORT/$DOCKER:newest 
+echo "pushing"
+docker push "$REGISTRY_HOST":"$REGISTRY_PORT"/"$DOCKER":"$VERSION"
