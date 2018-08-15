9e7bc60d635aef284250f4bb9ba72eee7923e639
Admir Trakic <atr@skm.dk>
2018-08-15 15:46:11 +0200

    Refactor groovy files

--- jenkins-server/local.sh
+++ jenkins-server/local.sh
@@ -5,15 +5,12 @@ SCRIPTPATH=$(dirname $SCRIPT)
 DOCKER=$(basename $SCRIPTPATH)
 TAG="$(git rev-parse --abbrev-ref HEAD)"
 
-# https://damnhandy.com/2016/03/06/creating-containerized-build-environments-with-the-jenkins-pipeline-plugin-and-docker-well-almost/
 docker run --name "$DOCKER" \
   -p 8080:8080 \
   -d \
   --restart=always \
-  --env JAVA_OPTS=-Djenkins.install.runSetupWizard=false \
   -v $(which docker):/bin/docker \
   -v "/var/run/docker.sock:/var/run/docker.sock:ro" \
   -v "$(pwd)":/var/jenkins_home/workspace/seed \
   -v "jenkins_home:/var/jenkins_home" \
   "$DOCKER:$TAG"
-
