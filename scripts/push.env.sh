701f27092303d7784088bca70fa5bb8319411770
Admir Trakic <xomodo@gmail.com>
2018-09-04 15:42:58 +0200

    Update scripts

--- scripts/push.env.sh
+++ scripts/push.env.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
 TOP="$(git rev-parse --show-toplevel)"
+
 . "$TOP/scripts/env.sh"
 
-#export DOCKER="$1"
 DOCKER=$(basename "$PWD")
-TOP="$(git rev-parse --show-toplevel)"
 TAG="$(git rev-parse --abbrev-ref HEAD)"
 
-if [ "$DOCKER_REGISTRY_LOCAL_LOGIN" ]; then 
-  echo "pushing docker: $DOCKER:$VERSION to $REGISTRY_HOST"
+if [ "$DOCKER_REGISTRY_LOCAL_LOGIN" ]; then
+  . "$TOP/scripts/login.env.sh"
+  echo "Pushing image: $DOCKER:$VERSION to registry: $REGISTRY_HOST"
   docker push "$REGISTRY_HOST":"$REGISTRY_PORT"/"$DOCKER":"$VERSION"
 fi
 
-#if [ "$DOCKER_ID_USER" ]; then
-  echo "Pushing $DOCKER:$VERSION to docker hub:"
+if [ "$DOCKER_ID_USER" ]; then
+  echo "Pushing image $DOCKER:$VERSION to docker hub:"
   docker tag "$DOCKER":"$VERSION" "$DOCKER_ID_USER"/"$DOCKER"
-  docker push "$DOCKER_ID_USER"/"$DOCKER"	
-#fi
+  docker push "$USER"/"$DOCKER:$VERSION"
+fi
