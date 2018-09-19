cb90fc44deae4a2f415c7e379ccd1781b6b36fee
Admir Trakic <atr@ufst.dk>
2018-09-19 15:34:42 +0200

    Use volume

--- jenkins-server/local.sh
+++ jenkins-server/local.sh
@@ -5,12 +5,13 @@ SCRIPTPATH=$(dirname $SCRIPT)
 DOCKER=$(basename $SCRIPTPATH)
 TAG="$(git rev-parse --abbrev-ref HEAD)"
 
+docker volume create "${DOCKER}"-data
+
 docker run --name "$DOCKER" \
   -p 8080:8080 \
-  -d \
-  --restart=always \
-  -v $(which docker):/bin/docker \
-  -v "/var/run/docker.sock:/var/run/docker.sock:ro" \
-  -v "$(pwd)":/var/jenkins_home/workspace/seed \
-  -v "jenkins_home:/var/jenkins_home" \
+  -v "$(which docker)":"/bin/docker"\
+  -v "/var/run/docker.sock":"/var/run/docker.sock:ro"\
+  -v "jenkins_home:/var/jenkins_home"\
+  -v "$(pwd)":"/var/jenkins_home/workspace/seed"\
+  -v "${DOCKER}"-data:"/var/jenkins_home"\
   "$DOCKER:$TAG"
